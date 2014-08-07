c6541a10d2e96e94f5ca19324c1cef0aad617ec0
Marco Agner <marco.agner@live.com>
2014-08-07 05:50:32 -0300

    fixes typo

--- examples/example.py
+++ examples/example.py
@@ -6,7 +6,7 @@ QRcode(app)
 
 @app.route('/')
 def index():
-    return render_template('examples.html')
+    return render_template('example.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
