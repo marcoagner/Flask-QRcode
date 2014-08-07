a2fd8b3e4ba6f9c964bb20557645c7b4f73da14c
Marco Agner <marco.agner@live.com>
2014-08-07 05:11:26 -0300

    creates example directory

---
+++ example/example.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask.ext.qrcode import QRcode
+
+app = Flask(__name__, template_folder='.')
+QRcode(app)
+
+@app.route('/')
+def index():
+    return render_template('example.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
