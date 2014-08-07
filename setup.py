30b0333a3d6925a087d2014ed853364015761592
Marco Agner <marco.agner@live.com>
2014-08-07 06:43:12 -0300

    bumps setup.py minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-QRcode',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='A simple flask extension to render QR codes on template',
     long_description=open('README.md').read(),
