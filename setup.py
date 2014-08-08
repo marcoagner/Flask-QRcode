18aa2b5bb8ce3e48acd7cda655ffa3de21aaa257
Marco Agner <marco.agner@live.com>
2014-08-08 15:37:01 -0300

    bumps setup.py patch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-QRcode',
-    version='0.3.0',
+    version='0.3.1',
     license='MIT',
     description='A simple flask extension to render QR codes on template',
     long_description=open('README.md').read(),
--- tests/test_qrcode.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-__author__ = 'marcoagner'
-
-
-class TestQrcode(TestCase):
-    pass
