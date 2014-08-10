0b5334adf3f4e23493f4727c1a5a65d0128116ee
Marco Agner <marco.agner@live.com>
2014-08-10 18:42:34 -0300

    treats multiple filenames when filename is None and filedir is .

--- flask_qrcode/__init__.py
+++ flask_qrcode/__init__.py
@@ -1,10 +1,16 @@
 import os.path
+import random
+import string
 
 from flask import render_template, Blueprint, Markup, url_for
 
 import qrcode as qrc
 
-def qrcode(data, filename='QRcode', filedir='.', version=None, error_correction=qrc.constants.ERROR_CORRECT_L, box_size=10, border=0, fit=True):
+createdQRs = {}
+
+def qrcode(data, filename=None, filedir='.', version=None, error_correction=qrc.constants.ERROR_CORRECT_L, box_size=10, border=0, fit=True):
+    currentQR = locals()
+    createFile = bool
     qr = qrc.QRCode(
         version=version,
         error_correction=error_correction,
@@ -14,14 +20,37 @@ def qrcode(data, filename='QRcode', filedir='.', version=None, error_correction=
     qr.add_data(data)
     qr.make(fit=fit)
 
-    qrpath = url_for('static', filename=os.path.join(filedir, filename) + '.png' )
-
     try:
-        with open('.' + qrpath, 'wb') as qrfile:
-            qrimg = qr.make_image()
-            qrimg.save(qrfile)
-    except IOError as err:
-        raise err("Could not create QR code file")
+        pop_createdQR = createdQRs[data].copy()
+        pop_createdQR.pop('filename')
+        pop_currentQR = currentQR.copy()
+        pop_currentQR.pop('filename')
+    except:
+        pass
+
+    if filename == None and data in createdQRs and pop_createdQR == pop_currentQR and os.path.isfile('.' + url_for('static', filename=os.path.join(filedir, createdQRs[data]['filename']))):
+        filename = createdQRs[data]['filename']
+        createFile = False
+    elif filename == None:
+        suffix = ''.join([random.choice(string.ascii_letters + string.digits + '-_') for ch in range(12)])
+        filename = 'QRcode_' + suffix + '.png'
+        createFile = True
+    else:
+        filename = filename + '.png'
+        createFile = True
+
+    qrpath = url_for('static', filename=os.path.join(filedir, filename))
+
+    if createFile is True:
+        createdQRs[data] = currentQR
+        createdQRs[data]['filename'] = filename
+
+        try:
+            with open('.' + qrpath, 'wb') as qrfile:
+                qrimg = qr.make_image()
+                qrimg.save(qrfile)
+        except IOError as err:
+            raise err("Could not create QR code file")
 
     return Markup(render_template('qrcode/qrcode.html', qrcode_url=qrpath))
 
