1295d24982492513e40dc4094255d874615cd83f
Marco Agner <marcoagner.dev@gmail.com>
2015-11-06 14:25:30 -0200

    updates setup.py for PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-QRcode',
-    version='0.5.1',
+    version='0.6.0',
     license='MIT',
     description='A simple flask extension to render QR codes on template',
     long_description=open('README.md').read(),
     author='Marco Agner',
-    author_email='dev@marcoagner.me',
+    author_email='hello@agner.io',
     url='https://github.com/marcoagner/Flask-QRcode',
     platforms='any',
     zip_safe=False,
@@ -23,7 +23,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
