b68400a5d55df98a110535eb7e52a6e3df752cb0
1dot75cm <sensor.wen@gmail.com>
2016-10-01 07:25:39 +0800

    Insert small image to QR Code
    Signed-off-by: 1dot75cm <sensor.wen@gmail.com>

--- sample_application/sample_application.py
+++ sample_application/sample_application.py
@@ -1,15 +1,26 @@
 # coding=utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
 
-from flask.ext.qrcode import QRcode
+from flask_qrcode import QRcode
+
 
 app = Flask(__name__)
-QRcode(app)
+qrcode = QRcode(app)
 
 
 @app.route('/')
 def index():
     return render_template('sample_application.html')
+
+
+@app.route('/qrcode', methods=['GET'])
+def get_qrcode():
+    # please get /qrcode?data=<qrcode_data>
+    data = request.args.get('data', '')
+    return send_file(
+        qrcode(data, mode='raw'),
+        mimetype='image/png'
+    )
 
 
 if __name__ == '__main__':
