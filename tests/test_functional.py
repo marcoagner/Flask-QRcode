6ba17d7a83c4f13ad83dd1d98a45fc85a1588dac
1dot75cm <sensor.wen@gmail.com>
2016-11-25 15:51:41 +0800

    Add testing suite
    Signed-off-by: 1dot75cm <sensor.wen@gmail.com>

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,7 +1,24 @@
 # coding=utf-8
-from flask_qrcode import qrcode
+import os
+from flask_qrcode import QRcode
+
+basedir = os.path.abspath(os.getcwd())
+testdir = os.path.join(basedir, 'tests')
 
 
-def test_create_qrcode():
-    new_qr = qrcode('Test Data')
+def test_load_online_icon_qrcode():
+    qrcode = QRcode()
+    old_qr = qrcode('Test Data')
+    new_qr = qrcode('Test Data',
+                    icon_img='https://avatars2.githubusercontent.com/u/5016303')
     assert "data:image/png;base64," in new_qr
+    assert len(new_qr) > len(old_qr)
+
+
+def test_load_local_icon_qrcode():
+    qrcode = QRcode()
+    old_qr = qrcode('Test Data')
+    new_qr = qrcode('Test Data',
+                    icon_img=os.path.join(testdir, 'icon.jpg'))
+    assert "data:image/png;base64," in new_qr
+    assert len(new_qr) > len(old_qr)
