59adcfe718e381ef21a1efabd805a4b050836232
marcoagner <marco@agner.io>
2016-12-30 19:29:54 -0200

    licenses go GPLv3 and updates version, setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+'''
+
+ Copyright (C) 2016 Marco Agner
+
+ This file is part of Flask-QRcode (v0.10.0).
+
+ Flask-QRcode (v0.10.0) is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Flask-QRcode (v0.10.0) is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
 import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
@@ -17,9 +37,9 @@ class PyTest(TestCommand):
 
 setup(
     name='Flask-QRcode',
-    version='0.9.0',
-    license='MIT',
-    description='A simple flask extension to render QR codes on template',
+    version='0.10.0',
+    license='GPLv3',
+    description='An elegant flask extension to render QR codes',
     long_description=open('README.md').read(),
     author='Marco Agner',
     author_email='marco@agner.io',
@@ -38,12 +58,14 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPLv3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
