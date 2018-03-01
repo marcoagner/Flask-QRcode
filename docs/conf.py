8f638b87c6d858e11b641509cb953c850f444169
marcoagner <marco@agner.io>
2018-03-01 19:12:31 +0000

    bumps version minor for doc bugfix

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@ copyright = '2014-2018, Marco Agner'
 author = 'Marco Agner'
 
 # The short X.Y version
-version = '1.1.1'
+version = '1.1.2'
 # The full version, including alpha/beta/rc tags
-release = 'v1.1.1'
+release = 'v1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
