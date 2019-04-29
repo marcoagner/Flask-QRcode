20efd9b3bf794f5b305a17bcc58b18aa320446b7
marcoagner <marco@agner.io>
2019-04-29 18:36:22 +0100

    formats code according to black formatter's default

--- flask_qrcode/__init__.py
+++ flask_qrcode/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 
  Copyright (C) 2016 Marco Agner
 
@@ -17,11 +17,12 @@
  You should have received a copy of the GNU General Public License
  along with Flask-QRcode.  If not, see <http://www.gnu.org/licenses/>.
 
-'''
+"""
 # coding=utf-8
 import os
 import base64
 from io import BytesIO
+
 try:
     from urllib import urlopen  # py2
 except:
@@ -35,15 +36,30 @@ from flask import Blueprint
 class QRcode(object):
     """QR Code image class"""
 
-    color = ['red', 'maroon', 'olive', 'yellow', 'lime', 'green',
-             'aqua', 'teal', 'blue', 'navy', 'fuchsia', 'purple',
-             'white', 'silver', 'gray', 'black']
+    color = [
+        "red",
+        "maroon",
+        "olive",
+        "yellow",
+        "lime",
+        "green",
+        "aqua",
+        "teal",
+        "blue",
+        "navy",
+        "fuchsia",
+        "purple",
+        "white",
+        "silver",
+        "gray",
+        "black",
+    ]
 
     correction_levels = {
-        'L': qrc.constants.ERROR_CORRECT_L,
-        'M': qrc.constants.ERROR_CORRECT_M,
-        'Q': qrc.constants.ERROR_CORRECT_Q,
-        'H': qrc.constants.ERROR_CORRECT_H
+        "L": qrc.constants.ERROR_CORRECT_L,
+        "M": qrc.constants.ERROR_CORRECT_M,
+        "Q": qrc.constants.ERROR_CORRECT_Q,
+        "H": qrc.constants.ERROR_CORRECT_H,
     }
 
     def __init__(self, app=None, config_jinja=True, **kwargs):
@@ -55,32 +71,41 @@ class QRcode(object):
 
     def __call__(self, *args, **kwargs):
         if self.app:
-            return self.qrcode(
-                static_dir=self.app.static_folder, *args, **kwargs)
+            return self.qrcode(static_dir=self.app.static_folder, *args, **kwargs)
         return self.qrcode(*args, **kwargs)
 
     def init_app(self, app):
         self.app = app
         self.register_blueprint(app)
 
-        if not hasattr(app, 'extensions'):
+        if not hasattr(app, "extensions"):
             app.extensions = {}
-        app.extensions['qrcode'] = self
+        app.extensions["qrcode"] = self
 
         if self._config_jinja:
             self._qrcode = self.__call__
-            app.add_template_filter(self._qrcode, 'qrcode')
-            app.add_template_global(self._qrcode, 'qrcode')
+            app.add_template_filter(self._qrcode, "qrcode")
+            app.add_template_global(self._qrcode, "qrcode")
 
     def register_blueprint(self, app):
-        module = Blueprint('qrcode', __name__, template_folder='templates')
+        module = Blueprint("qrcode", __name__, template_folder="templates")
         app.register_blueprint(module)
         return module
 
     @classmethod
-    def qrcode(cls, data, mode="base64", version=None, error_correction="L",
-               box_size=10, border=0, fit=True, fill_color="black",
-               back_color="white", **kwargs):
+    def qrcode(
+        cls,
+        data,
+        mode="base64",
+        version=None,
+        error_correction="L",
+        box_size=10,
+        border=0,
+        fit=True,
+        fill_color="black",
+        back_color="white",
+        **kwargs
+    ):
         """Makes qr image using qrcode as qrc. See documentation
         for qrcode (https://pypi.python.org/pypi/qrcode) package for more info.
 
@@ -102,33 +127,39 @@ class QRcode(object):
             version=version,
             error_correction=cls.correction_levels[error_correction],
             box_size=box_size,
-            border=border
+            border=border,
         )
         qr.add_data(data)
         qr.make(fit=fit)
 
-        fcolor = fill_color if fill_color.lower() in cls.color or \
-            fill_color.startswith('#') else "#"+fill_color
-        bcolor = back_color if back_color.lower() in cls.color or \
-            back_color.startswith('#') else "#"+back_color
+        fcolor = (
+            fill_color
+            if fill_color.lower() in cls.color or fill_color.startswith("#")
+            else "#" + fill_color
+        )
+        bcolor = (
+            back_color
+            if back_color.lower() in cls.color or back_color.startswith("#")
+            else "#" + back_color
+        )
 
         # creates qrcode base64
         out = BytesIO()
         qr_img = qr.make_image(back_color=bcolor, fill_color=fcolor)
         qr_img = qr_img.convert("RGBA")
         qr_img = cls._insert_img(qr_img, **kwargs)
-        qr_img.save(out, 'PNG')
+        qr_img.save(out, "PNG")
         out.seek(0)
 
-        if mode == 'base64':
-            return u"data:image/png;base64," +\
-                   base64.b64encode(out.getvalue()).decode('ascii')
-        elif mode == 'raw':
+        if mode == "base64":
+            return u"data:image/png;base64," + base64.b64encode(out.getvalue()).decode(
+                "ascii"
+            )
+        elif mode == "raw":
             return out
 
     @staticmethod
-    def _insert_img(qr_img, icon_img=None, factor=4, icon_box=None,
-                    static_dir=None):
+    def _insert_img(qr_img, icon_img=None, factor=4, icon_box=None, static_dir=None):
         """Inserts a small icon to QR Code image"""
         img_w, img_h = qr_img.size
         size_w = int(img_w) / int(factor)
@@ -140,7 +171,7 @@ class QRcode(object):
             if static_dir:
                 # load icon from app's static dir
                 icon_fp = os.path.join(static_dir, icon_img)
-            if icon_img.split('://')[0] in ['http', 'https', 'ftp']:
+            if icon_img.split("://")[0] in ["http", "https", "ftp"]:
                 icon_fp = BytesIO(urlopen(icon_img).read())  # download icon
             icon = Image.open(icon_fp)
         except:
@@ -150,11 +181,10 @@ class QRcode(object):
         icon_w = size_w if icon_w > size_w else icon_w
         icon_h = size_h if icon_h > size_h else icon_h
         icon = icon.resize((int(icon_w), int(icon_h)), Image.ANTIALIAS)
-        icon = icon.convert('RGBA')
+        icon = icon.convert("RGBA")
 
         left = int((img_w - icon_w) / 2)
         top = int((img_h - icon_h) / 2)
-        icon_box = (int(icon_box[0]), int(icon_box[1])) if icon_box else\
-                   (left, top)
+        icon_box = (int(icon_box[0]), int(icon_box[1])) if icon_box else (left, top)
         qr_img.paste(im=icon, box=icon_box, mask=icon)
         return qr_img
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-'''
+"""
 
  Copyright (C) 2016 Marco Agner
 
@@ -17,7 +17,7 @@
  You should have received a copy of the GNU General Public License
  along with Flask-QRcode.  If not, see <http://www.gnu.org/licenses/>.
 
-'''
+"""
 import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
@@ -26,52 +26,49 @@ from setuptools.command.test import test as TestCommand
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['-v']
+        self.test_args = ["-v"]
         self.test_suite = True
 
     def run_tests(self):
         import pytest
+
         err_code = pytest.main(self.test_args)
         sys.exit(err_code)
 
 
 setup(
-    name='Flask-QRcode',
-    version='3.0.0',
-    license='GPLv3',
-    description='A concise Flask extension to render QR codes on Jinja2 ' \
-                'templates using python-qrcode',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Marco Agner',
-    author_email='marco@agner.io',
-    url='https://github.com/marcoagner/Flask-QRcode',
-    platforms='any',
+    name="Flask-QRcode",
+    version="3.0.0",
+    license="GPLv3",
+    description="A concise Flask extension to render QR codes on Jinja2 "
+    "templates using python-qrcode",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Marco Agner",
+    author_email="marco@agner.io",
+    url="https://github.com/marcoagner/Flask-QRcode",
+    platforms="any",
     zip_safe=False,
     include_package_data=True,
-    install_requires=[
-        'Flask',
-        'qrcode',
-        'pillow'
-    ],
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+    install_requires=["Flask", "qrcode", "pillow"],
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    packages=find_packages()
+    packages=find_packages(),
 )
